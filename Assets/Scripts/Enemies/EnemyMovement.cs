9fed17de75135a8ac212b2023e9dc39231841020
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-09-30 16:10:31 +0200

    Kind of a working enemy movement

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -11,16 +12,56 @@ namespace Enemies
         [SerializeField] private EnemyStatsSo stats;
 
         [SerializeField] private GameObject player;
+        private bool dangerZone;
+        private CharacterController controller;
+
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
 
         private void Update()
         {
-            transform.LookAt(player.transform);
+            Look();
             Move();
         }
 
         void Move()
         {
+            Vector3 direction = player.transform.position - transform.position;
+            Vector3 velocity = direction * stats.speed / 10;
             
+            if (!dangerZone)
+            {
+                controller.Move(velocity * Time.deltaTime);
+
+            }
+            else
+            {
+                controller.Move(velocity * (-5 * Time.deltaTime));
+            }
+        }
+
+        void Look()
+        {
+            transform.LookAt(player.transform);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y ,0);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                dangerZone = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                dangerZone = false;
+            }
         }
     }
 }
