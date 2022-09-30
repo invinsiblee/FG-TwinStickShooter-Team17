6d2f290123029412e6a013ef23d87dc119e29412
EmilGyllendahl <gyllendahlgyllendahl@hotmail.com>
2022-09-30 15:17:29 +0200

    Testing list
    Trying out making a list so that more objects easily could be added to the spawner mechanic. It is not working properly right now with the Instantiate.

--- Assets/Scripts/Environment/RandomSpawner.cs
+++ Assets/Scripts/Environment/RandomSpawner.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RandomSpawner : MonoBehaviour
 {
-    public GameObject cubeprefab; // The variable for the chosen object to spawn.
+   public GameObject cubeprefab; // The variable for the chosen object to spawn.
+
+    [SerializeField] private List<GameObject> objectsspawning; // This list is not working properly yet. I am having problems with the Instantiate part in my if statement.
+
+
 
     // Update is called once per frame
     void Update()
@@ -13,6 +18,7 @@ public class RandomSpawner : MonoBehaviour
         {
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-85, 85), 5, Random.Range(-87, 87)); // Randomises the spawn position with the x/y/z axis also horizontially and vertically.
             Instantiate(cubeprefab, randomSpawnPosition, Quaternion.identity);
+            
         }
     }
 }
