4e2ebd67cc71e74491af95f0f939d9595c51c7f1
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-09-30 09:27:21 +0200

    Our own scenes

---
+++ Assets/Scripts/Enemies/EnemyStatsSo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Enemies
+{
+    [CreateAssetMenu(fileName = "EnemyStats", menuName = "ScriptableObjects/EnemyStats", order = 1)]
+    public class EnemyStatsSo : ScriptableObject
+    {
+        public float health;
+        public float speed;
+        public float damage;
+        public float bulletsPerSecond;
+    }
+}
