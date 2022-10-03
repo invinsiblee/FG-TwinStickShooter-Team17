d10a75466de341dbbd8d1410e19a6d64cb56cca1
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-10-03 09:47:54 +0200

    RandomSpawner wokring

--- Assets/Scripts/Environment/RandomSpawner.cs
+++ Assets/Scripts/Environment/RandomSpawner.cs
@@ -6,45 +6,35 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour
 {
     public GameObject[] spawnGameObject; // The variable for the chosen object or objects to spawn.
+    [Header("Spawn timer")]
     public float spawnWait;
     public float spawnMostWait;
     public float spawnLeastWait;
-    public int startWait;
-    public bool stop;
-    int randomObject;
+    
+    [Header("Number of Gamebjects in scene")]
+    [SerializeField] private int totalObjects;
+    public int currentObjects;
 
-    private void Start()
+    void Update()
     {
-        StartCoroutine(waitSpawner());
+        Spawn();
+        spawnWait -= 1 * Time.deltaTime;
     }
 
+    void Spawn()
+    {
+        // Takes and cycles through the random indexes of the items in the array.
+        int randimIndex = Random.Range(0, spawnGameObject.Length);
 
-        void Update()
+        // Randomises the spawn position with the x/y/z axis also horizontially and vertically.
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-85, 85), 5, Random.Range(-87, 87));
+
+        if (currentObjects < totalObjects && spawnWait <= 0)
         {
-                                                                    //if(Input.GetKeyDown(KeyCode.Space)) // When spacekey is pressed spawn items
-             spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
-
-            // Takes and cycles through the random indexes of the items in the array.
-            int randimIndex = Random.Range(0, spawnGameObject.Length);
-
-            // Randomises the spawn position with the x/y/z axis also horizontially and vertically.
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-85, 85), 5, Random.Range(-87, 87));
-
-            // Instansiates the spawnning of the game objects and the randomSpawnPosition
+            //Random time
+            spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+            //Spawn object
             Instantiate(spawnGameObject[randimIndex], randomSpawnPosition, Quaternion.identity);
-        
-            
-           
-         }
-
-            IEnumerator waitSpawner()
-            {
-                yield return new WaitForSeconds(startWait);
-
-                while (!stop)
-                {
-                   
-                    yield return new WaitForSeconds(spawnWait);
-                }
-            }
+        }
+    }
 }
