b4eb6ebec77c4c610aacd97450bf33e6c7aa37d0
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-10-03 10:21:49 +0200

    Destroy what exits the bounds

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] ObjectsToSpawn
+        ;
+    [Header("Spawn timer")]
+    [HideInInspector] 
+    [SerializeField] private float spawnWait;
+    [SerializeField] private float spawnMostWait;
+    [SerializeField] private float spawnLeastWait;
+    
+    [Header("Number of Gamebjects in scene")]
+    [SerializeField] private int totalObjects;
+    public int currentObjects;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Spawn();
+        spawnWait -= 1 * Time.deltaTime;
+    }
+
+    void Spawn()
+    {
+        // Takes and cycles through the random indexes of the items in the array.
+        int randomIndex = Random.Range(0, ObjectsToSpawn.Length);
+
+        // Randomises the spawn position with the x/y/z axis also horizontially and vertically.
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-85, 85), 5, Random.Range(-87, 87));
+
+        if (currentObjects < totalObjects && spawnWait <= 0)
+        {
+            //Random time
+            spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+            //Spawn object
+            Instantiate(ObjectsToSpawn[randomIndex], randomSpawnPosition, Quaternion.identity);
+        }
     }
 }
