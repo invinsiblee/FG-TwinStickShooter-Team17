36b5e2872e2d493b396f73db9f71604e8ffe8fc5
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-10-04 10:29:57 +0200

    Removed unnecessary namespaces

--- Assets/Scripts/Enemies/Bullet/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet/Bullet.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Enemies;
+
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private EnemyStatsSo stats;
--- Assets/Scripts/Enemies/EnemyCombat.cs
+++ Assets/Scripts/Enemies/EnemyCombat.cs
@@ -1,39 +1,36 @@
 using System;
 using UnityEngine;
 
-namespace Enemies
+public class EnemyCombat : MonoBehaviour
 {
-    public class EnemyCombat : MonoBehaviour
+    [SerializeField] private EnemyStatsSo stats;
+    [SerializeField] private Transform bulletPosition;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private AudioSource shoot;
+
+    private float currentFrame;
+
+    private void Start()
     {
-        [SerializeField] private EnemyStatsSo stats;
-        [SerializeField] private Transform bulletPosition;
-        [SerializeField] private GameObject bullet;
-        [SerializeField] private AudioSource shoot;
+        shoot = GameObject.Find("EnemyShoot").GetComponent<AudioSource>();
+    }
 
-        private float currentFrame;
+    private void Update()
+    {
+        SpawnBullet();
+        currentFrame -= 1 * Time.deltaTime;
+    }
 
-        private void Start()
+    void SpawnBullet()
+    {
+        if (currentFrame <= 0)
         {
-            shoot = GameObject.Find("EnemyShoot").GetComponent<AudioSource>();
-        }
-
-        private void Update()
-        {
-            SpawnBullet();
-            currentFrame -= 1 * Time.deltaTime;
-        }
-
-        void SpawnBullet()
-        {
-            if (currentFrame <= 0)
-            {
-                var BulletSpawn = new Vector3(bulletPosition.position.x, bulletPosition.position.y,
-                    bulletPosition.position.z);
-                currentFrame = stats.timeUntilNextShot;
-                //instantiate 
-                shoot.Play();
-                Instantiate(bullet, BulletSpawn, new Quaternion()); 
-            }
+            var BulletSpawn = new Vector3(bulletPosition.position.x, bulletPosition.position.y,
+                bulletPosition.position.z);
+            currentFrame = stats.timeUntilNextShot;
+            //instantiate 
+            shoot.Play();
+            Instantiate(bullet, BulletSpawn, new Quaternion()); 
         }
     }
 }
