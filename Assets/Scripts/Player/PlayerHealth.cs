784506de23740305d65a9e02988fa9fb4a798cc2
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-10-04 11:11:25 +0200

    Health script done

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public bool mortal = true;
+    [HideInInspector] public bool mortal = true;
+    
+    [SerializeField] private int maxHealth;
+    [HideInInspector] public float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    void Update()
+    {
+        CheckPlayerHealth();
+    }
+
+    void CheckPlayerHealth()
+    {
+        //healthBar.SetCurrentHealth(currentHealth);
         
+        if (currentHealth <= 0)
+        {
+            //Dead
+            currentHealth = 0;
+        }
+    }
 }
