d15b222914f749f977eef90827cd469192d84072
EmilGyllendahl <gyllendahlgyllendahl@hotmail.com>
2022-10-05 12:22:50 +0200

    Added variables position values to the spawner.

--- Assets/Scripts/Environment/RandomSpawner.cs
+++ Assets/Scripts/Environment/RandomSpawner.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour
 {
     public GameObject[] spawnGameObject; // The variable for the chosen object or objects to spawn.
+    public GameObject[] spawningposition;
     [Header("Spawn timer")]
-    
-    [HideInInspector] 
+
+    [HideInInspector]
     public float spawnWait;
     public float spawnMostWait;
     public float spawnLeastWait;
-    
+
     [Header("Number of Gamebjects in scene")]
     [SerializeField] private int totalObjects;
     public int currentObjects;
 
+
+    [Header("Values for the spawning position")]
+    [SerializeField] private float xAxis;
+    [SerializeField] private float zAxis;
+    [SerializeField] private float yAxis;
+    [SerializeField] private float xAxisVertically;
+    [SerializeField] private float zAxisVertically;
     void Update()
     {
         Spawn();
         spawnWait -= 1 * Time.deltaTime;
     }
+
+   
 
     void Spawn()
     {
@@ -28,14 +38,14 @@ public class RandomSpawner : MonoBehaviour
         int randimIndex = Random.Range(0, spawnGameObject.Length);
 
         // Randomises the spawn position with the x/y/z axis also horizontially and vertically.
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-65, 65), 5, Random.Range(-60, 60));
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(xAxis, zAxis), yAxis, Random.Range(xAxisVertically, zAxisVertically));
 
         if (currentObjects < totalObjects && spawnWait <= 0)
         {
             //Random time
             spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
             //Spawn object
-            Instantiate(spawnGameObject[randimIndex], randomSpawnPosition, Quaternion.identity);
+            Instantiate(spawnGameObject [randimIndex], randomSpawnPosition, Quaternion.identity);
         }
     }
 }
